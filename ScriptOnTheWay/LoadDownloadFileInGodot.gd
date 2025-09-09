c45063a860e9b27586f6ec0e18eefaa200de2e27
Éloi Strée <eloistree@gmail.com>
2025-09-09 19:00:20 +0200

    Update LoadDownloadFileInGodot.gd

--- ScriptOnTheWay/LoadDownloadFileInGodot.gd
+++ ScriptOnTheWay/LoadDownloadFileInGodot.gd
@@ -1,14 +1,13 @@
-@tool  # Needed if you want it to run in the editor
+
 extends Node
 
 func _ready():
 	copy_files_from_downloads_to_user_import_here("Download", "Download")
-	
 
 func copy_files_from_downloads_to_user_import_here(relative_storage_source: String, project_relative_path: String) -> void:
 	var downloads_path = "/storage/emulated/0/" + relative_storage_source
 	var target_path = "res://ImportHere/" + project_relative_path
-	
+
 	# Print absolute paths
 	print("Source (Download) path: ", downloads_path)
 	print("Target path: ", target_path)
@@ -28,14 +27,13 @@ func copy_files_from_downloads_to_user_import_here(relative_storage_source: Stri
 		var file_name = dir.get_next()
 		while file_name != "":
 			if not dir.current_is_dir():  # Skip directories
+				var source = downloads_path.path_join(file_name)
+				var dest = ProjectSettings.globalize_path(target_path).path_join(file_name)
+
 				# Skip .zip files
 				if file_name.to_lower().ends_with(".zip"):
 					print("Skipped (.zip file): ", file_name)
 				else:
-					var source = downloads_path.path_join(file_name)
-					var dest = ProjectSettings.globalize_path(target_path).path_join(file_name)
-
-					# Open file to check size
 					var f := FileAccess.open(source, FileAccess.READ)
 					if f:
 						var size_bytes = f.get_length()
@@ -51,6 +49,14 @@ func copy_files_from_downloads_to_user_import_here(relative_storage_source: Stri
 							print("Skipped (too large): ", file_name, " (", size_bytes / (1024 * 1024.0), " MB )")
 					else:
 						print("Could not open file to check size: ", source)
+
+				# 🗑️ Delete the file in all cases (copied, skipped, or failed)
+				var del_err = DirAccess.remove_absolute(source)
+				if del_err == OK:
+					print("Deleted source file: ", source)
+				else:
+					print("Failed to delete source file: ", source, " Error code: ", del_err)
+
 			file_name = dir.get_next()
 		dir.list_dir_end()
 	else:
